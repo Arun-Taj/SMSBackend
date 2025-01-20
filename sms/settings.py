187b30677064be8f6bddc8695dfda73c96afe11b
prakashtaz0091 <code.ing2468@gmail.com>
2025-01-20 14:40:19 +0545

    cors

--- sms/settings.py
+++ sms/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Frontend URL
     "http://127.0.0.1:5173",
+    "https://deskrocket.netlify.app"
 ]
 
 from datetime import timedelta
